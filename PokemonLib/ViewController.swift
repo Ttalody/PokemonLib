2bd2ac850fc1a01f7d56f170eb508c3c963258da
Artur Akulich <akulich.artur@gmail.com>
2023-10-16 21:57:51 +0300

    Initial commit

---
+++ PokemonLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonLib
+//
+//  Created by Артур on 16.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
